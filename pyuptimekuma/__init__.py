5d9584c8bae7fb0af3952aa6ba9b869b900a3185
Jayakorn Karikan <jayakornk@gmail.com>
2022-06-26 14:41:39 +0700

    Change versioning method

--- pyuptimekuma/__init__.py
+++ pyuptimekuma/__init__.py
@@ -1,5 +1,6 @@
 """Python API wrapper for Uptime Kuma."""
-from .__version__ import __version__
 from .exceptions import *
 from .models import *
 from .uptimekuma import UptimeKuma
+
+__version__ = '0.0.4'
